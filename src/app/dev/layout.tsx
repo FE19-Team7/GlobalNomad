97fbd9ca20956418c83de510413a2ce6320c9aeb
DongHyun <cdhppph@gamil.com>
2025-12-19 19:17:23 +0900

    feat: test페이지반영

--- src/app/dev/layout.tsx
+++ src/app/dev/layout.tsx
@@ -1,9 +1,16 @@
-import type { PropsWithChildren } from 'react';
+import { Footer } from "@/src/components/Footer/Footer";
+import type { PropsWithChildren } from "react";
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <div className="min-h-full py-12">
-      <div className="w-full max-w-[964px] mx-auto flex flex-col gap-y-8">{children}</div>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1 py-12">
+        <div className="w-full max-w-[964px] mx-auto flex flex-col gap-y-8">
+          {children}
+        </div>
+      </main>
+
+      <Footer />
     </div>
   );
 }
