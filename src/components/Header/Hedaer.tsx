af90c137882ce52e00b68b3ec153a2b7c1701e87
조동현 <cdhppph@gmail.com>
2025-12-22 19:16:11 +0900

    feat: 인증영역 구현

--- src/components/Header/Hedaer.tsx
+++ src/components/Header/Hedaer.tsx
@@ -8,17 +8,22 @@ type HeaderProps = {
 };
 
 export default function Header({ isLoggedIn = false }: HeaderProps) {
+  const nickname = "조동현"; // 임시 데이터 -> 추후 로그인 회원가입 연동 시 수정
   return (
     <HeaderLayout>
       <div className="grid w-full grid-cols-2 items-center">
         {/* 왼쪽 */}
-        <div className="flex items-center gap-2 justify-start cursor-pointer">
+        <div className="flex items-center justify-start cursor-pointer">
           <Logo />
         </div>
 
         {/* 오른쪽 */}
         <div className="flex justify-end">
-          {isLoggedIn ? <LoggedInMenu /> : <LoggedOutMenu />}
+          {isLoggedIn ? (
+            <LoggedInMenu nickname={nickname} />
+          ) : (
+            <LoggedOutMenu />
+          )}
         </div>
       </div>
     </HeaderLayout>
--- src/components/Header/LoggedInMenu.tsx
+++ src/components/Header/LoggedInMenu.tsx
@@ -1,13 +1,29 @@
 import BellIcon from "@/assets/icon_bell.svg";
 import DefaultProfile from "@/assets/default profile.svg";
+import Link from "next/link";
 
-export default function LoggedInMenu() {
+type LoggedInMenuProps = {
+  nickname: string;
+};
+
+export default function LoggedInMenu({ nickname }: LoggedInMenuProps) {
   return (
     <div className="flex items-center gap-2.5">
-      <BellIcon />
-      <div className="h-3.5 w-px bg-gray-100" />
-      <DefaultProfile />
-      <span className="text-sm text-gray-950">조동현</span>
+      <Link
+        href="/notifications"
+        aria-label="알림"
+        className="hover:opacity-70 transition"
+      >
+        <BellIcon />
+      </Link>
+      <Link
+        href="/mypage"
+        aria-label="마이페이지"
+        className="flex items-center gap-2 hover:opacity-70 transition"
+      >
+        <DefaultProfile />
+        <span className="text-sm text-gray-950">{nickname}</span>
+      </Link>
     </div>
   );
 }
