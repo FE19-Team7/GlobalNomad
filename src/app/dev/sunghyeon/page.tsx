39920ad7109e808aec263e4adbbd2507ca2d365e
glowhyeon <shyeon710@gmail.com>
2025-12-25 04:07:07 +0900

    feat: activity 카드 컴포넌트 구현 (#39)

--- src/app/dev/sunghyeon/page.tsx
+++ src/app/dev/sunghyeon/page.tsx
@@ -1,5 +1,15 @@
+import ActivityCard from "../../../components/ActivityCard/ActivityCard";
+
 export default function SunghyeonPage() {
   return (
-    <div>테스트용 페이지</div>
+    <div className="flex flex-row gap-6">
+      <ActivityCard
+        id={1}
+        title="크리스마스 랜선 집들이"
+        rating={4.9}
+        reviewCount={10}
+        price={25000}
+      />
+    </div>
   );
 }
