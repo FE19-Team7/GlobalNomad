cd960c91e4d19ff8c5de585e638b4a59dfba49eb
Kim Junyeol <kennen1771@gmail.com>
2025-12-30 03:53:06 +0900

    chore: axios기반 api구조 http및 index 추가(#57)

---
+++ src/api/index.ts
@@ -0,0 +1,11 @@
+export { http } from './http';
+
+export * from './activities/activities.api';
+export * from './auth/auth.api';
+export * from './categories/categories.api';
+export * from './myActivities/myActivities.api';
+export * from './myReservations/myReservations.api';
+export * from './notifications/notifications.api';
+export * from './reviews/reviews.api';
+export * from './oauth/oauth.api';
+export * from './users/users.api';
