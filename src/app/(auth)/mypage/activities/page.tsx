005689635a3aae88a134c5c555345f21d666b7b7
Kim Junyeol <kennen1771@gmail.com>
2026-01-10 14:12:09 +0900

    feat: 체험 상세 임시 페이지 (#163)

--- src/app/(auth)/mypage/activities/page.tsx
+++ src/app/(auth)/mypage/activities/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-export default function ActivityDetailPage() {
-  return (
-    <div>
-      체험 상세 페이지
-    </div>
-  );
+import React from 'react';
+
+export default function Page() {
+  return <div />;
 }
