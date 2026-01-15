46d4accf5d17ea40470ca8cbca046dc6042b874c
glowhyeon <shyeon710@gmail.com>
2026-01-16 03:15:37 +0900

    feat: 메인 페이지 search 컴포넌트 적용 (#214)

--- src/features/mainpage/components/SearchSection.tsx
+++ src/features/mainpage/components/SearchSection.tsx
@@ -1,7 +1,22 @@
+'use client';
+
+import { useState } from 'react';
+import SearchBar from '@/src/components/Search/Search';
+
 export default function SearchSection() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term: string) => {
+    console.log('검색:', term);
+  };
+
   return (
     <section className="w-full h-[208px]">
-
+      <SearchBar
+        value={searchTerm}
+        onChange={setSearchTerm}
+        onSearch={handleSearch}
+      />
     </section>
   );
 }
